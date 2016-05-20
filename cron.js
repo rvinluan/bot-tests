62e6ae3a426510eb24a4fad3f5ef39e0781ea4ea
Robert Vinluan <robert.vinluan@gmail.com>
2016-05-19 21:59:52 -0400

    full functionality

--- cron.js
+++ cron.js
@@ -2,6 +2,6 @@ var CronJob = require('cron').CronJob;
 
 var LikeHashtags = require('./LikeHashtags');
 
-new CronJob('* * * * * *', function() {
+new CronJob('00 00 12 * * *', function() {
   LikeHashtags.run();
 }, null, true, 'America/Los_Angeles');
