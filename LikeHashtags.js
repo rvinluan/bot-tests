835a814b6815d064199194826ab599e25324f623
Robert Vinluan <robert.vinluan@gmail.com>
2016-05-19 21:34:13 -0400

    added twit api config

--- LikeHashtags.js
+++ LikeHashtags.js
@@ -1,7 +1,11 @@
-module.exports = {
-  run: run
-}
+var Twit = require('twit');
+var T = new Twit(require('./config.js'));
+
 
 function run() {
   console.log('hi');
+}
+
+module.exports = {
+  run: run
 }
